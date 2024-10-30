dc01a84a6583c211ede8d9388eccdc4db84bb78f
Dario213987 <dariocrsng@gmail.com>
2024-10-30 18:09:57 -0300

    Se agregó la clase is-invalid a al formulario cliente

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -15,28 +15,28 @@
             @csrf
             <div class="mb-3">
               <label for="dni" class="form-label">DNI</label>
-              <input type="number" placeholder="DNI sin puntos..." name="dni" class="form-control" aria-describedby="emailHelp">
+              <input type="number" placeholder="DNI sin puntos..." name="dni" class="form-control @error('dni') is-invalid @enderror" aria-describedby="emailHelp">
               @error('dni')
                 <small class="text-danger"> {{ '*'.$message}}</small>
             @enderror
             </div>
             <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre completo</label>
-                <input type="text" name="nombre" class="form-control" aria-describedby="emailHelp">
+                <input type="text" name="nombre" class="form-control @error('nombre') is-invalid @enderror" aria-describedby="emailHelp">
                 @error('nombre')
                 <small class="text-danger"> {{ '*'.$message}}</small>
             @enderror
             </div>
             <div class="mb-3">
                 <label for="contacto" class="form-label">Contacto</label>
-                <input type="text" name="contacto" class="form-control" aria-describedby="emailHelp">
+                <input type="text" name="contacto" class="form-control @error('contacto') is-invalid @enderror" aria-describedby="emailHelp">
                 @error('contacto')
                 <small class="text-danger"> {{ '*'.$message}}</small>
             @enderror
             </div>
             <div class="mb-3">
                 <label for="domicilio" class="form-label">Domicilio</label>
-                <input type="text" name="domicilio" class="form-control" aria-describedby="emailHelp">
+                <input type="text" name="domicilio" class="form-control @error('domicilio') is-invalid @enderror" aria-describedby="emailHelp">
                 @error('domicilio')
                 <small class="text-danger"> {{ '*'.$message}}</small>
             @enderror
@@ -44,7 +44,7 @@
             <div class="mb-3">
                 <label for="socio" class="form-label">
                     Es socio
-                    <input type="checkbox" value="1" name="socio" class="checkbox-control" aria-describedby="emailHelp">
+                    <input type="checkbox" value="1" name="socio" class="checkbox-control @error('socio') is-invalid @enderror" aria-describedby="emailHelp">
                 </label>
                 @error('socio')
                 <small class="text-danger"> {{ '*'.$message}}</small>
