2262d017d9821d9b975216be9538fe343f75815f
Facuelchea32 <144394190+Facuelchea32@users.noreply.github.com>
2024-11-04 17:10:58 -0300

    vista create.descuentos
    vista descuentos - facundo

---
+++ resources/views/descuentos/create.blade.php
@@ -0,0 +1,37 @@
+@extends('template')
+@section('titulo', 'Panel')
+@push('css')
+
+@endpush
+@section('contenido')
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Turnos</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a class="text-decoration-none" href="{{ route('panel') }} ">Panel</a></li>
+            <li class="breadcrumb-item"><a class="text-decoration-none" href="{{ route('Descuentos') }} ">Descuentos</a></li>
+            <li class="breadcrumb-item active">Crear</li>
+        </ol>
+        <form method="POST" action="{{ route('descuento-guardar') }}">
+            @csrf
+            <div class="mb-3">
+              <label for="nombre" class="form-label">Nombre</label>
+              <input type="text"  name="nombre" class="form-control @error('nombre') is-invalid @enderror" aria-describedby="emailHelp">
+              @error('nombre')
+                <small class="text-danger"> {{ '*'.$message}}</small>
+            @enderror
+            </div>
+            <div class="mb-3">
+                <label for="cantidad" class="form-label">Cantidad</label>
+                <input type="text"  name="cantidad" class="form-control @error('cantidad') is-invalid @enderror" aria-describedby="emailHelp">
+                @error('nombre')
+                  <small class="text-danger"> {{ '*'.$message}}</small>
+            @enderror
+            </div>
+            
+            <a href="{{ route('descuentos') }}" class="btn btn-secondary">Cancelar</a>
+            <button type="submit" class="btn btn-primary">Crear</button>
+          </form>
+    </div>
+@endsection
+@push('js')
+@endpush
