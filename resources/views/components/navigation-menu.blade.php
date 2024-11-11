496af1daf34105785615ad01f56dfb17ca891893
devmobi1812 <144266743+devmobi1812@users.noreply.github.com>
2024-11-11 01:48:00 -0300

    Update navigation-menu.blade.php

--- resources/views/components/navigation-menu.blade.php
+++ resources/views/components/navigation-menu.blade.php
@@ -83,6 +83,10 @@
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-list"></i></div>
                     Depositos
                 </a>
+                <a class="nav-link" href="">
+                    <div class="sb-nav-link-icon"><i class="fa-solid fa-credit-card"></i></div>
+                    Metodos de Pago
+                </a>
                 <a class="nav-link" href="{{ route('usuarios')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-user fa-fw"></i></div>
                     Usuarios
