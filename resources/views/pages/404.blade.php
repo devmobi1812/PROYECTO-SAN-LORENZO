2407d672f5da30067ce3938d78a6f4a436460966
devmobi1812 <144266743+devmobi1812@users.noreply.github.com>
2024-11-13 00:36:05 -0300

    Footer actualizado

--- resources/views/pages/404.blade.php
+++ resources/views/pages/404.blade.php
@@ -34,11 +34,11 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div class="text-muted">&copy; 2024 Club San Lorenzo by TAS</div>
                             <div>
-                                <a href="#">Privacy Policy</a>
+                                <a href="{{route("politicas")}}">Politica de Pivacidad</a>
                                 &middot;
-                                <a href="#">Terms &amp; Conditions</a>
+                                <a href="{{route("terminos")}}">Terminos &amp; Condiciones</a>
                             </div>
                         </div>
                     </div>
