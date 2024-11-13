13db94fc1147130c3e5fc75e778e29fe62c1c519
devmobi1812 <144266743+devmobi1812@users.noreply.github.com>
2024-11-12 23:58:52 -0300

    Update create.blade.php

--- resources/views/descuentos/create.blade.php
+++ resources/views/descuentos/create.blade.php
@@ -14,7 +14,7 @@
             @csrf
             <div class="mb-3">
               <label for="nombre" class="form-label">Nombre</label>
-              <input type="text" placeholder="Ingrese el nombre del descuento (ej: %30)" name="nombre" class="form-control @error('nombre') is-invalid @enderror" aria-describedby="emailHelp">
+              <input type="text" placeholder="Ingrese el nombre del descuento (ej: 30%)" name="nombre" class="form-control @error('nombre') is-invalid @enderror" aria-describedby="emailHelp">
               @error('nombre')
                 <small class="text-danger"> {{ '*'.$message}}</small>
             @enderror
